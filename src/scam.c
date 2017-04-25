ed1e32f39f6d2639bf4d79d91f07ff3e9c442511
Ian Fisher <ianfisher45@gmail.com>
2017-04-25 17:21:23 -0400

    Modify command line options of interpreter

--- src/scam.c
+++ src/scam.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "collector.h"
 #include "eval.h"
 
@@ -8,22 +9,36 @@ void run_repl(scamval*);
 
 int main(int argc, char** argv) {
     scamval* env = scamdict_builtins();
+    char* cvalue = NULL;
     int load_flag = 0;
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-l") == 0) {
-            load_flag = 1;
-        } else {
-            scamval* v = eval_file(argv[i], env);
-            if (v->type == SCAM_ERR) {
+    int c;
+    while ((c = getopt(argc, argv, "ic:")) != -1) {
+        switch (c) {
+            case 'i':
+                load_flag = 1;
+                break;
+            case 'c':
+                cvalue = optarg;
+                scamval* v = eval_str(cvalue, env);
                 scamval_println(v);
-            }
-            gc_unset_root(v);
+                return 0;
+            case '?':
+                return 1;
+            default:
+                break;
         }
+    }
+    // evaluate files
+    for (int i = optind; i < argc; i++) {
+        scamval* v = eval_file(argv[i], env);
+        if (v->type == SCAM_ERR) {
+            scamval_println(v);
+        }
+        gc_unset_root(v);
     }
     if (load_flag || argc == 1) {
         run_repl(env);
     }
-    gc_close();
     return 0;
 }
 
---
+++ todo.md
@@ -0,0 +1,20 @@
+# Short term
+- Finish standard library
+- Ensure that define cannot appear within expressions
+- Much more standard library tests
+- Update documentation
+- Garbage collector should mark free memory instead of actually freeing it
+- Better error messages
+
+# Bugs
+- none that I know of (probably a bad sign)
+
+# Long term
+- Error handling
+- Unicode support
+
+# Possibilities
+- Module system
+- Object-oriented programming
+- Variadic functions
+- Convert AST to stack machine instructions???!!!
