1608d4b4dee21e77565a3326572ccade8177f6ab
Ian Fisher <ianfisher45@gmail.com>
2017-05-03 19:36:04 -0400

    Fix bug in test_all.sh
    Lo and behold the testing script wasn't actually working, which actually
    explains a lot.

--- test_all.sh
+++ test_all.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-function invoke_valgrind {
-	valgrind -q --leak-check=full --show-leak-kinds=all --num-callers=500 $1
-}
-
-function invoke_run_test_script {
-	invoke_valgrind ./run_test_script $1
-}
-
-invoke_valgrind ./tests
-invoke_run_test_script resources/test_stdlib.scm
-invoke_run_test_script resources/test_core.scm
-invoke_run_test_script resources/test_escapes.scm
-invoke_run_test_script resources/test_programs.scm
+valgrind -q --leak-check=full --show-leak-kinds=all --num-callers=500 ./tests
+valgrind -q --leak-check=full --show-leak-kinds=all --num-callers=500 ./run_test_script resources/test_stdlib.scm
+valgrind -q --leak-check=full --show-leak-kinds=all --num-callers=500 ./run_test_script resources/test_core.scm
+valgrind -q --leak-check=full --show-leak-kinds=all --num-callers=500 ./run_test_script resources/test_escapes.scm
+valgrind -q --leak-check=full --show-leak-kinds=all --num-callers=500 ./run_test_script resources/test_programs.scm
